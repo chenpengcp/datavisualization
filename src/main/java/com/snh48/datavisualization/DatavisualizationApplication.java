81bb49bb844c96a65c3890582a1ffce031e1f1ef
chenpengcp <chen_peng_cp@126.com>
2019-04-30 15:12:10 +0800

    asdasdas

--- src/main/java/com/snh48/datavisualization/DatavisualizationApplication.java
+++ src/main/java/com/snh48/datavisualization/DatavisualizationApplication.java
@@ -2,8 +2,10 @@ package com.snh48.datavisualization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DatavisualizationApplication {
 
     public static void main(String[] args) {
